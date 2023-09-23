c2156c05b6ede6bb432e492eedc2c9906eccd80e
jsnmhm <jsn.mhm@gmail.com>
2023-09-22 21:46:45 -0400

    color change

--- docs/app.js
+++ docs/app.js
@@ -130,7 +130,7 @@ function barChart(ids, values, labels) {
         text: labels.slice(0, 10).reverse(),
         marker: {
             color: values,
-            colorscale: "Cividis"
+            colorscale: "Blues"
         }
     };
 
@@ -152,7 +152,7 @@ function animateBarChart(ids, values, labels) {
         text: labels.slice(0, 10).reverse(),
         marker: {
             color: values,
-            colorscale: "Cividis"
+            colorscale: "Blues"
         }
     };
 
